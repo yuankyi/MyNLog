804cdc84908633fcd8d49627565a5fe15d8916e1
yuanky <yuanky123@qq.com>
2021-12-28 09:04:35 +0800

    添加项目文件。

---
+++ MyNLog/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyNLog
+{
+    class Program
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        private static readonly NLog.Logger mylogger = NLog.LogManager.GetLogger("mylogger");
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                logger.Info("Hello world");
+                //Structured logging is recommended, e.g.
+                logger.Info("Hello {Name}", "Earth");
+                //Or using string-format syntax:
+                logger.Info("Hello {0}", "Moon");
+
+                try
+                {
+                    int i = 0, j = 1;
+                    var xx = j / i;
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex, "Goodbye cruel world");
+                    mylogger.Error(ex, "my error ..");
+                }
+
+                Console.ReadKey();
+            }
+            while (true);
+        }
+    }
+}
